a48d418085ca9b399a74b212f44605d6d8e5745c
Prasundas99 <prasundas115@gmail.com>
2025-01-16 02:29:30 +0530

    svelte rewrite

--- client/src/redux/actionTypes/notesConstants.js
+++
@@ -1,5 +0,0 @@
-export const FETCH_ALL = 'FETCH_ALL';
-export const CREATE = 'CREATE';
-export const UPDATE = 'UPDATE';
-export const DELETE = 'DELETE';
-export const UPDATEDETAILS = 'UPDATEDETAILS'
